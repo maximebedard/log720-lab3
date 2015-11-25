2e519829a3567b7a760b06944deff3cae0b2e455
Maxime Bedard <maxime.bedard@shopify.com>
2015-11-25 10:01:25 -0500

    Comment all the security things yayaz

--- src/main/java/ca/etsmtl/log720/lab3/controllers/AuthenticationsController.java
+++ src/main/java/ca/etsmtl/log720/lab3/controllers/AuthenticationsController.java
@@ -1,28 +1,29 @@
-package ca.etsmtl.log720.lab3.controllers;
+//package ca.etsmtl.log720.lab3.controllers;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Controller
+//@RequestMapping("/auth")
+//public class AuthenticationsController {
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    private String login(){
+//        return "auth/login";
+//    }
+//
+//    @RequestMapping(value= "/logout", method = RequestMethod.GET)
+//    private String logout(HttpServletRequest request, HttpServletResponse response){
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth != null){
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//        }
+//        return "redirect:/login?logout";
+//    }
+//}
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Controller
-@RequestMapping("/auth")
-public class AuthenticationsController {
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    private String login(){
-        return "auth/login";
-    }
-
-    @RequestMapping(value= "/logout", method = RequestMethod.GET)
-    private String logout(HttpServletRequest request, HttpServletResponse response){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/login?logout";
-    }
-}
