8d72cf8d5cda988886879479ec02d4577e09049a
Maxime Bedard <maxime.bedard@shopify.com>
2015-11-27 07:40:16 -0500

    Revert "Comment all the security things yayaz"
    This reverts commit 2e519829a3567b7a760b06944deff3cae0b2e455.

--- src/main/java/ca/etsmtl/log720/lab3/SecurityConfiguration.java
+++ src/main/java/ca/etsmtl/log720/lab3/SecurityConfiguration.java
@@ -1,35 +1,35 @@
 package ca.etsmtl.log720.lab3;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//import javax.sql.DataSource;
-//
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery(getUsersByUsernameQuery())
-//                .authoritiesByUsernameQuery(getAuthoritiesByUsernameQuery());
-//    }
-//
-//    private String getUsersByUsernameQuery() {
-//        return "select username, pwd as password" +
-//                "from users" +
-//                "where username = ?";
-//    }
-//
-//    private String getAuthoritiesByUsernameQuery() {
-//        return "select username, role as authority" +
-//                "from users" +
-//                "inner join user_roles on users.id = user_roles.user_id" +
-//                "inner join roles on user_roles.role_id = roles.id" +
-//                "where username = ?";
-//    }
-//}
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(getUsersByUsernameQuery())
+                .authoritiesByUsernameQuery(getAuthoritiesByUsernameQuery());
+    }
+
+    private String getUsersByUsernameQuery() {
+        return "select username, pwd as password" +
+                "from users" +
+                "where username = ?";
+    }
+
+    private String getAuthoritiesByUsernameQuery() {
+        return "select username, role as authority" +
+                "from users" +
+                "inner join user_roles on users.id = user_roles.user_id" +
+                "inner join roles on user_roles.role_id = roles.id" +
+                "where username = ?";
+    }
+}
